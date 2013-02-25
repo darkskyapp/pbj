b72de4c9c3f6eaf50c1dd70b78a5da599fdd22c2
Jason LaPorte <ironwallaby@gmail.com>
2013-02-25 15:33:00 -0500

    Test.

--- test.js
+++ test.js
@@ -7,7 +7,7 @@
 
   describe("PBJ", function() {
     it("should read a gzipped pbj from disk and let the user look up bits", function(done) {
-      PBJ.readFile(path.join(__dirname, "test.pbj.gz"), function(err, pbj) {
+      PBJ.readFile(path.join(__dirname, "testdata/1.pbjz"), function(err, pbj) {
         expect(err).to.be.null;
         expect(pbj).to.be.an.instanceOf(PBJ);
         expect(pbj.width()).to.equal(128);
@@ -17,6 +17,18 @@
         expect(pbj.bitAt(32, -1)).to.be.false;
         expect(pbj.bitAt(30, 49)).to.be.false;
         expect(pbj.bitAt(28, 80)).to.be.true;
+        done();
+      });
+    });
+
+    it("should read a different gzipped pbj from disk and let the user look up bits", function(done) {
+      PBJ.readFile(path.join(__dirname, "testdata/1.pbjz"), function(err, pbj) {
+        expect(err).to.be.null;
+        expect(pbj).to.be.an.instanceOf(PBJ);
+        expect(pbj.width()).to.equal(128);
+        expect(pbj.height()).to.equal(128);
+        expect(pbj.bitAt(68, 67)).to.be.true;
+        expect(pbj.bitAt(67, 68)).to.be.true;
         done();
       });
     });
